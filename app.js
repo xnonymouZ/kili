bb283f47ffbb99577632029507df0ff77a8471da
Shin Maung Maung <ethereal97@gmail.com>
2020-05-29 05:17:32 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 
 function converter(event) {
-  var x = event.which || event.keyCode;
-  if(x>=65&&x<=122){
-    var cValue = document.getElementById("cValue");
-    var filtered=data.filter(function(item){
+  try {
+    var key = event.which || event.keyCode;
+    if(key > =65 && key <= 122){
+      var cValue = document.getElementById("cValue");
+      var filtered = data.filter(function(item){
         return item.en === cValue.value
       });
-      if(filtered.length>0)
-      document.getElementById("demo").innerHTML = "The MM  value is: " + filtered[0].mm;
+      if(filtered.length > 0) {
+        document.getElementById("demo").innerHTML = "The MM  value is: " + filtered[0].mm;
+      }
+    }
+  } catch (e) {
+     alert(e.message);
   }
-
 }
+
+windiw.addEventListener('load', () => {
+    let input = document.querySelector('textarea')
+    input.addEventListener('keyup', converter)
+})
+
